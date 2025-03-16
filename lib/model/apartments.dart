1cf587fb66f4839b571aaac900170af9919bc545
Humberto2379 <70221019+Humberto2379@users.noreply.github.com>
2025-03-16 13:34:39 -0400

    add likes to apartments, starting with 0

--- lib/model/apartments.dart
+++ lib/model/apartments.dart
@@ -6,13 +6,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Apartment {
   Future<void> addApartment(aTitle, aTown, aPrice, aNeighborhood, aType, aGender,aSummary, uID) async {
     DocumentReference<Map<String, dynamic>> docRef = await FirebaseFirestore.instance.collection('Apartments').add({
-      'Apartment_title': aTitle,
+      'Apartment title': aTitle,
       'Town': aTown,
       'Price': aPrice,
       'Neighborhood': aNeighborhood,
       'Type': aType,
       'Gender':aGender,
       'Summary':aSummary,
+      'Likes': 0,
       'uID' : uID,
     });
     
